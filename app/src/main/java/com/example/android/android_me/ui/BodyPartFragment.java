4dd130a6c0094259b3fa5e7e99ec2355b2097fed
Mirka <mirka.gajdosova@gmail.com>
2017-10-23 22:39:50 +0100

    Add BodyPartFragment

---
+++ app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -0,0 +1,10 @@
+package com.example.android.android_me.ui;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Mirka on 23/10/2017.
+ */
+
+public class BodyPartFragment extends Fragment {
+}
