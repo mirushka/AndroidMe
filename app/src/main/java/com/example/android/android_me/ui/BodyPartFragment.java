ac17fefce73c4441607f3fd714d7f7169a40f6b5
Mirka <mirka.gajdosova@gmail.com>
2017-10-24 20:30:41 +0100

    Create Body branch

--- app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
+++ app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -1,6 +1,14 @@
 package com.example.android.android_me.ui;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
 
 /**
  * Created by Mirka on 23/10/2017.
@@ -11,6 +19,17 @@ public class BodyPartFragment extends Fragment {
 //    Empty constructor
     public BodyPartFragment(){
 
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+
+        View rootView = inflater.inflate(R.layout.fragment_body_part, container, false);
+
+        ImageView imageView = (ImageView) rootView.findViewById(R.id.body_part_image_view);
+        imageView.setImageResource(AndroidImageAssets.getHeads().get(0));
+        return rootView;
     }
 
 
